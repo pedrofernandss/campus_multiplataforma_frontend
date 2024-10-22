1feacba6f9269ab145bb57edb25eb84d4b450b5d
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2024-10-21 21:58:39 -0300

    :card_file_box: raw: add mocked datas for the feed carousel

---
+++ assets/mocked-data/carouselImages.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        id:'1',
+        image: require('./images/image1.png'),
+    },
+    {
+        id:'2',
+        image: require('./images/image2.png'),
+    },
+    {
+        id:'3',
+        image: require('./images/image3.png'),
+    },
+];
