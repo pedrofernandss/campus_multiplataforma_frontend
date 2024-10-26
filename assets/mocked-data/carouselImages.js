3c7272bcde83bd8d91bf65d88c955d8c2bee8722
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2024-10-26 15:54:54 -0300

    :wrench: chore: mocking carousel images

--- assets/mocked-data/carouselImages.js
+++ assets/mocked-data/carouselImages.js
@@ -1,14 +1,18 @@
 export default [
     {
         id:'1',
-        image: require('./images/image1.png'),
+        image: require('./images/image1.jpg'),
     },
     {
         id:'2',
-        image: require('./images/image2.png'),
+        image: require('./images/image1.jpg'),
     },
     {
         id:'3',
-        image: require('./images/image3.png'),
+        image: require('./images/image1.jpg'),
+    },
+    {
+        id:'4',
+        image: require('./images/image1.jpg'),
     },
 ];
