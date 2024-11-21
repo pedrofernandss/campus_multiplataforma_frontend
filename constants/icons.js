1bea13417b9f204d395e58169ef792d5bbe49b28
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2024-11-21 17:59:43 -0300

    ➕ chore: add standard constants

--- constants/icons.js
+++ constants/icons.js
@@ -1,5 +1,4 @@
 import menuIcon from "../assets/icons/menu-icon.png";
-import searchIcon from "../assets/icons/search-icon.png";
 import trendingIcon from "../assets/icons/trending-up-icon.png"
 
-export default { menuIcon, searchIcon, trendingIcon };
+export default { menuIcon, trendingIcon };
--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,5 @@
 import icons from "./icons";
 import images from "./images";
+import standard from "./standardThemes"
 
-export { icons, images };
+export { icons, images, standard };
