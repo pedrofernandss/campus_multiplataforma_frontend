ceaed1e0f33f4621625f9164e85b8ae568b31726
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2024-11-22 18:50:14 -0300

    🐛 fix: fixing safearea bug

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,13 @@
-import Header from "@/components/Header";
-import Trendings from "@/components/Trendings"
-import React, { useEffect, useRef } from 'react';
+import Header from "../components/Header";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Carousel from "@/components/Carousel";
 
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <Header />
-      <Trendings />
-      <Carousel />
 
-      <StatusBar className="bg-campusRed"/>
+      <StatusBar backgroundColor='bg-campusRed' style='dark' />
     </SafeAreaView>
   );
 }
