9853d7328d32dbc444d1d7ed6171ffbd3b73853b
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2024-11-30 00:21:24 -0300

    🗃️ feat: add masonry list mocked data

---
+++ assets/mocked-data/masonryImages.js
@@ -0,0 +1,47 @@
+export default [
+    {
+        id:'1',
+        image: require('./images/image2.jpg'),
+        aspectRatio: 1,
+    },
+    {
+        id:'2',
+        image: require('./images/image2.jpg'),
+        aspectRatio: 200 / 145,
+    },
+    {
+        id:'3',
+        image: require('./images/image2.jpg'),
+        aspectRatio: 180 / 145,
+    },
+    {
+        id:'4',
+        image: require('./images/image2.jpg'),
+        aspectRatio: 210 / 145,
+    },
+    {
+        id:'5',
+        image: require('./images/image2.jpg'),
+        aspectRatio: 160 / 145,
+    },
+    {
+        id:'6',
+        image: require('./images/image2.jpg'),
+        aspectRatio: 1,
+    },
+    {
+        id:'7',
+        image: require('./images/image2.jpg'),
+        aspectRatio: 120 / 145,
+    },
+    {
+        id:'8',
+        image: require('./images/image2.jpg'),
+        aspectRatio: 210 / 145,
+    },
+    {
+        id:'9',
+        image: require('./images/image2.jpg'),
+        aspectRatio: 120 / 145
+    },
+];
