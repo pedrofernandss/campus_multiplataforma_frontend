47b79a00e002a6cc0fdb15ec9c05f15aa8a5cd6e
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2024-12-11 23:25:28 -0300

    ✨ feat: fetch youtube videos with youtube api

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,25 +1,55 @@
+import axios from 'axios'
+import { YOUTUBE_API_KEY } from '@env';
+import React, { useEffect, useState } from 'react'
 import { View, FlatList, Dimensions, Animated } from 'react-native'
-import React from 'react'
-import carouselImages from '@/assets/mocked-data/carouselImages'
 import CarouselCard from './CarouselCard'
 import Paginator from './Paginator'
 
 const {width} = Dimensions.get('window')
 const scrollX = new Animated.Value(0);
 
+
 const Carousel = () => {
-  
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const url = 'https://www.googleapis.com/youtube/v3/search'
+
+    const fetchVideos = async () => {
+      try {
+        const response = await axios.get(url, {
+          params: {
+            part: 'snippet',
+            channelId: 'UChegNOjzApuGBTQyPo3I27Q',
+            order: 'date',
+            maxResults: 4,
+            key: YOUTUBE_API_KEY,
+            type: 'video',          
+            videoDuration: 'medium'
+          }
+        });
+        console.log('Tipo da variável videos:', typeof videos);
+        setVideos(response.data.items);
+      } catch (error) {
+        const err = error as Error;
+        console.error('API Error:', err.message);
+      }
+    };
+
+    fetchVideos();
+  }, []);
+
   return (
     <View>
       <FlatList
       horizontal
       pagingEnabled
       showsHorizontalScrollIndicator={false}
-      snapToOffsets={[...Array(carouselImages.length)].map((_, i) => i * (width * 0.8-40) + (i-1)*40)}
+      snapToOffsets={[...Array(videos.length)].map((_, i) => i * (width * 0.8-40) + (i-1)*40)}
       snapToAlignment={'center'}
       scrollEventThrottle={16}
-      initialScrollIndex={1}
-      getItemLayout={(data, index) => ({
+      initialScrollIndex={(videos.length/2)-1}
+      getItemLayout={(videos, index) => ({
         length: width * 0.8 - 40,
         offset: (width * 0.8 - 40) * index,
         index,
@@ -29,10 +59,11 @@ const Carousel = () => {
       })}
       decelerationRate={'fast'}
       style={{marginTop:10}}
-      data={carouselImages} renderItem={({ item }) => <CarouselCard item={item}/> }
-      keyExtractor={(item) => item.id}
+      data={videos} renderItem={({ item }) => <CarouselCard item={item}/> }
+      keyExtractor={(item) => item.id.videoId}
+      
     />
-    <Paginator data={carouselImages} scrollX={scrollX} />
+    <Paginator data={videos} scrollX={scrollX} />
     </View>
   )
 }
