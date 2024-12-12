f0601a609caf5828876f9db793a58a60e6e9c6ff
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2024-12-11 23:25:54 -0300

    ✨ feat: pass videos and redirect to link

--- components/CarouselCard.tsx
+++ components/CarouselCard.tsx
@@ -1,18 +1,32 @@
-import { StyleSheet, Image, View, Dimensions } from 'react-native'
+import { Linking, StyleSheet, Image, View, Dimensions, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { LinearGradient } from 'expo-linear-gradient'
 
 const {width} = Dimensions.get('window')
 
 const CarouselCard = ({ item }) => {
+  const videoLink = `https://www.youtube.com/watch?v=${item.id.videoId}`
+
+  const goToVideo = async () => {
+    if (videoLink) {
+      const supported = await Linking.canOpenURL(videoLink);
+      if (supported) {
+        await Linking.openURL(videoLink);
+      } else {
+        console.error(`Não é possível abrir o link: ${item.link}`);
+      }
+    }
+};
+
   return (
-    <View style={styles.container}>
-      <Image source={item.image} style={styles.image} />
+    <TouchableOpacity style={styles.container} onPress={goToVideo}>
+      
+      <Image source={{ uri: item.snippet.thumbnails.medium.url }} style={styles.image} />
       <LinearGradient
         colors={['transparent', 'rgba(0, 0, 0, 0.5)']}
         style={styles.gradient}
       />
-    </View>
+    </TouchableOpacity>
   )
 }
 
@@ -23,8 +37,8 @@ const styles = StyleSheet.create({
         position: 'relative',
     },
     image: {
-        height: width/2.6,
         width: width*0.8-20,
+        height: width/2.6,
         marginHorizontal:10,
         borderRadius: 12,
     },
